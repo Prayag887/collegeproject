5f5e881709794f2d898620f580a5c51958b864aa
Prayag887 <prayagdhakal123@gmail.com>
2024-02-26 07:05:27 +0545

    New Changes

--- project/frontends/main.js
+++ project/frontends/main.js
@@ -1,4 +1,4 @@
-// // Throttle function
+// Throttle function
 // function throttle(fn, wait) {
 //   var time = Date.now();
 //   return function() {
@@ -9,7 +9,7 @@
 //   };
 // }
 
-// // Parallax for moon
+// Parallax for moon
 // window.addEventListener('scroll', throttle(parallaxMoon, 14));
 
 // function parallaxMoon() {
@@ -20,17 +20,17 @@
 //   moon.style.transform = 'translate(' + rev + ', ' + coords + ')';
 // }
 
-// // Parallax for mountains
+// Parallax for mountains
 // window.addEventListener('scroll', throttle(parallaxMountains, 14));
 
 // function parallaxMountains() {
-//   var scrolled = window.pageYOffset;
+  // var scrolled = window.pageYOffset;
 //   var mountains = document.querySelector(".mountains_behind");
 //   var coords = (scrolled * 0.2) + 'px';
 //   mountains.style.transform = 'translateY(' + coords + ')';
 // }
 
-// // Parallax for stars
+// Parallax for stars
 // window.addEventListener('scroll', throttle(parallaxStars, 14));
 
 // function parallaxStars() {
@@ -39,10 +39,6 @@
 //   var coords = (scrolled * 0.9) + 'px';
 //   stars.style.transform = 'translateX(' + coords + ')';
 // }
-
-
-
-
 
 
 
